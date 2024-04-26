8b20287fc6abfd81a314d32cce4c60a76ed64a3a
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-04-27 02:41:55 +0600

    show all item api added

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors());
+app.use(cors(
+
+  {
+    origin: ["http://localhost:5173", "https://art-and-craft-store.netlify.app/"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    withCredentials: true,
+  }
+
+))
 app.use(express.json());
 
 
