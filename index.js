6be664daa2e2c189ffbf92e9844c46425e9c69df
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-04-27 01:41:12 +0600

    delete api added

--- index.js
+++ index.js
@@ -76,6 +76,13 @@ async function run() {
     });
 
 
+    app.delete('/added/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollections.deleteOne(query);
+      res.send(result);
+    });
+
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
